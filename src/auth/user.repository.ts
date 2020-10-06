f7e3bfde0c7d4f3214cf2854fcaa2909d8042768
joerdis101 <joerdis@bites-it.de>
2020-10-06 13:11:40 +0200

    build: replace bcrypt with bcryptjs

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,6 @@
 import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { UserEntity } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
